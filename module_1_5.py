f18c900bca060ab637b383243b0452f8685354eb
Dmitry96024 <koluchi-92@mail.ru>
2024-09-08 18:00:58 +0500

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,18 @@
+immutable_var= 1, "apple", True
+print(immutable_var)
+# immutable_var[0] = 100 Вывод: TypeError: 'tuple' object does not support item assignment
+# но если сделать список то будет так:
+immutable_var_1=[(1,2,3), "apple", True]
+print(immutable_var_1)
+immutable_var_1[2]=False
+print(immutable_var_1)
+mutable_list=[1, "Dmitriy", False]
+print(mutable_list)
+mutable_list.remove(False)
+print(mutable_list)
+mutable_list.append("Alex")
+print(mutable_list)
+mutable_list.extend("Yuliya")
+mutable_list.extend(["Yuliya", "25"])
+print(mutable_list)
+print(immutable_var_1+mutable_list)
