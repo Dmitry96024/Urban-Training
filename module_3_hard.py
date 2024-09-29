d2fb3d03203dea312585342a5b3018400eb06655
Dmitry96024 <koluchi-92@mail.ru>
2024-09-29 21:05:31 +0500

    Create module_3_hard.py

---
+++ module_3_hard.py
@@ -0,0 +1,31 @@
+# Создаём функцию
+def calculate_structure_sum(data_structure):
+    summ = 0
+    # сумма key и value
+    if isinstance(data_structure, dict):
+        for key, value in data_structure.items():
+            summ += calculate_structure_sum(key)
+            summ += calculate_structure_sum(value)
+        # иначе сумма состоящаяя из списка,кортэжа,множества
+    elif isinstance(data_structure, (list, tuple, set)):
+        for item in data_structure:
+            summ += calculate_structure_sum(item)
+        # иначе сумма - целого числа и числа с плавающей точкой
+    elif isinstance(data_structure, (int, float)):
+        summ += data_structure
+        # summa - функции len
+    elif isinstance(data_structure, str):
+        summ += len(data_structure)
+        # возврат из функции значения 'summ'
+    return summ
+
+data_structure = [
+  [1, 2, 3],
+  {'a': 4, 'b': 5},
+  (6, {'cube': 7, 'drum': 8}),
+  "Hello",
+  ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+# вывод результата программы
+result = calculate_structure_sum(data_structure)
+print(result)
