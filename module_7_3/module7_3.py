541252e47704753311f9d8d121bf60d96726de35
Dmitriy <koluchi-92@mail.ru>
2024-11-01 20:36:13 +0500

    Create module7_3.py

---
+++ module_7_3/module7_3.py
@@ -0,0 +1,94 @@
+'''
+Напишите класс WordsFinder, объекты которого создаются следующим образом:
+WordsFinder('file1.txt, file2.txt', 'file3.txt', ...).
+Объект этого класса должен принимать при создании неограниченного количество названий файлов и записывать их в атрибут file_names в виде списка или кортежа.
+
+Также объект класса WordsFinder должен обладать следующими методами:
+get_all_words - подготовительный метод, который возвращает словарь следующего вида:
+{'file1.txt': ['word1', 'word2'], 'file2.txt': ['word3', 'word4'], 'file3.txt': ['word5', 'word6', 'word7']}
+Где:
+'file1.txt', 'file2.txt', ''file3.txt'' - названия файлов.
+['word1', 'word2'], ['word3', 'word4'], ['word5', 'word6', 'word7'] - слова содержащиеся в этом файле.
+Алгоритм получения словаря такого вида в методе get_all_words:
+Создайте пустой словарь all_words.
+Переберите названия файлов и открывайте каждый из них, используя оператор with.
+Для каждого файла считывайте единые строки, переводя их в нижний регистр (метод lower()).
+Избавьтесь от пунктуации [',', '.', '=', '!', '?', ';', ':', ' - '] в строке. (тире обособлено пробелами, это не дефис в слове).
+Разбейте эту строку на элементы списка методом split(). (разбивается по умолчанию по пробелу)
+В словарь all_words запишите полученные данные, ключ - название файла, значение - список из слов этого файла.
+
+find(self, word) - метод, где word - искомое слово. Возвращает словарь, где ключ - название файла, значение - позиция первого такого слова в списке слов этого файла.
+count(self, word) - метод, где word - искомое слово. Возвращает словарь, где ключ - название файла, значение - количество слова word в списке слов этого файла.
+В методах find и count пользуйтесь ранее написанным методом get_all_words для получения названия файла и списка его слов.
+Для удобного перебора одновременно ключа(названия) и значения(списка слов) можно воспользоваться методом словаря - item().
+
+for name, words in get_all_words().items():
+  # Логика методов find или count
+'''
+import io
+from pprint import pprint
+
+class WordsFinder:
+    def __init__(self, *file_names):
+        self.file_names = file_names
+
+    def get_all_words(self):
+        all_words = {}
+        for file_name in self.file_names:
+            with open(file_name, 'r', encoding='utf-8') as file:
+                words = []
+                for line in file:
+                    line = line.lower()
+                    punctuation = [',', '.', '=', '!', '?', ';', ':']
+                    for punct in punctuation:
+                        line = line.replace(punct, '')
+                    line = line.replace(' - ', ' ')
+                    words.extend(line.split())
+                all_words[file_name] = words
+        return all_words
+
+    def find(self, word):
+        places = {}
+        for key, value in self.get_all_words().items():
+            if word.lower() in value:
+                places[key] = value.index(word.lower()) + 1
+        return places
+
+    def count(self, word):
+        counters = {}
+        for value, key in self.get_all_words().items():
+            words_count = key.count(word.lower())
+            counters[value] = words_count
+        return counters
+
+print('======================================finger1===================================================')
+print('Поисковое слово: the')
+finder1 = WordsFinder('Walt Whitman - O Captain! My Captain!.txt',
+                      'Rudyard Kipling - If.txt',
+                      'Mother Goose - Monday’s Child.txt')
+print(finder1.get_all_words())
+print(finder1.find('the'))
+print(finder1.count('the'))
+print('======================================finger2===================================================')
+finder2 = WordsFinder('test_file.txt')
+print(finder2.get_all_words()) # Все слова
+print(finder2.find('TEXT')) # 3 слово по счёту
+print(finder2.count('teXT')) # 4 слова teXT в тексте всего
+print('======================================finger3===================================================')
+print('Поисковое слово: captain')
+finder3 = WordsFinder('Walt Whitman - O Captain! My Captain!.txt')
+print(finder1.get_all_words())
+print(finder1.find('captain'))
+print(finder1.count('captain'))
+print('======================================finger4===================================================')
+print('Поисковое слово: if')
+finder4 = WordsFinder('Rudyard Kipling - If.txt',)
+print(finder1.get_all_words())
+print(finder1.find('if'))
+print(finder1.count('if'))
+print('======================================finger5===================================================')
+print ('Поисковое слово: child')
+finder5 = WordsFinder('Mother Goose - Monday’s Child.txt',)
+print(finder1.get_all_words())
+print(finder1.find('Child'))
+print(finder1.count('Child'))
