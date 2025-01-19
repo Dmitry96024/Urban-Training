9beecc6aa69a6522d7451d70cac0e32accd4edc8
Dmitriy <koluchi-92@mail.ru>
2025-01-19 23:59:13 +0500

    Update module_14_4.py

--- module_14/module_14_4/module_14_4.py
+++ module_14/module_14_4/module_14_4.py
@@ -5,7 +5,7 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from crud_function import *
 
-api = ''
+api = '***'
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
